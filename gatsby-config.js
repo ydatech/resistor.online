98abbb04faa4c731149931c8af0b4642ebf445bb
Yuda Sukmana <yudasukmana1992@gmail.com>
2019-02-09 17:31:39 +0700

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Resistor Calculator`,
+    description: `Resistor Calculator will help you calculate resistor with 4/5/6 color bands value.`,
+    author: `@resistor.online`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,45 @@
 import React from "react"
-import { Link } from "gatsby"
+//import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+import Resistor4 from '../components/resistor4'
+import Resistor5 from '../components/resistor5'
+import Resistor6 from '../components/resistor6'
+
+class IndexPage extends React.Component {
+  state = {
+    mode: 4
+  }
+  render() {
+    const { mode } = this.state;
+    return (
+      <Layout>
+        <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+        <h1>Resistor Calculator</h1>
+        <div style={{
+
+          display: "inline-block",
+          position: "relative",
+          width: "100%",
+          paddingBottom: "100%",
+          verticalAlign: "middle",
+          overflow: "hidden"
+        }}>
+          {mode === 4 && <Resistor4
+          />}
+          {mode === 5 && <Resistor5
+          />}
+          {mode === 6 && <Resistor6
+          />}
+        </div>
+
+      </Layout>
+    )
+  }
+
+}
+
 
 export default IndexPage
