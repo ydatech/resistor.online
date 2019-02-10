9fdfdff9efba408a7b1aef517c8e2703d24f2418
Yuda Sukmana <yudasukmana1992@gmail.com>
2019-02-10 21:35:06 +0700

    update icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Resistor Calculator`,
+        short_name: `resistor-calculator`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
